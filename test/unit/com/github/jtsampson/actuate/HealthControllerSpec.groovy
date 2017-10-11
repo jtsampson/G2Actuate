0e61f170facaf42503d4ee15e1e8fda1f3993066
John Sampson <john.sampson@beeline.com>
2017-10-11 08:15:00 -0600

    Adding HealthController Test.

--- test/unit/com/github/jtsampson/actuate/HealthControllerSpec.groovy
+++ test/unit/com/github/jtsampson/actuate/HealthControllerSpec.groovy
@@ -21,43 +21,36 @@ import grails.test.mixin.TestFor
 import org.codehaus.groovy.grails.web.servlet.HttpHeaders
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
 @TestFor(HealthController)
 class HealthControllerSpec extends Specification {
 
-    def mockHealthService
-    def mockActuateSecurity
-    def collectHealthResponse
 
+    static def COLLECT_HEALTH_RESPONSE = [status                        : 'UP',
+                                          h2DataSourceHealthIndicator   : [status    : 'UP',
+                                                                           database  : 'H2',
+                                                                           version   : '1.3.176 (2014-04-05)',
+                                                                           driver    : 'org.h2.jdbc.JdbcConnection',
+                                                                           validation: 'select 1', validationResult: 1],
+                                          derbyDataSourceHealthIndicator: [status    : 'UP',
+                                                                           database  : 'Apache Derby',
+                                                                           version   : '10.12.1.1 -(1704137)',
+                                                                           driver    : 'org.apache.derby.impl.jdbc.EmbedConnection',
+                                                                           validation: 'SELECT 1 FROM SYSIBM.SYSDUMMY1', validationResult: 1]
+    ]
+
+    def mockHealthService
 
     def setup() {
-
         mockHealthService = mockFor(HealthService)
-        mockActuateSecurity = mockFor(ActuateSecurity)
-        collectHealthResponse = [status                        : 'UP',
-                                 h2DataSourceHealthIndicator   : [status    : 'UP',
-                                                                  database  : 'H2',
-                                                                  version   : '1.3.176 (2014-04-05)',
-                                                                  driver    : 'org.h2.jdbc.JdbcConnection',
-                                                                  validation: 'select 1', validationResult: 1],
-                                 derbyDataSourceHealthIndicator: [status    : 'UP',
-                                                                  database  : 'Apache Derby',
-                                                                  version   : '10.12.1.1 -(1704137)',
-                                                                  driver    : 'org.apache.derby.impl.jdbc.EmbedConnection',
-                                                                  validation: 'SELECT 1 FROM SYSIBM.SYSDUMMY1', validationResult: 1]
-        ]
     }
 
-    void "test insecure request does not invoke security"() {
+    void "request is unsecure"() {
         given:
-        mockHealthService.demand.collectHealth() { -> return collectHealthResponse }
+        mockHealthService.demand.collectHealth() { -> return COLLECT_HEALTH_RESPONSE }
 
         request.secure = false
         request.addHeader(HttpHeaders.ACCEPT, JSON_CONTENT_TYPE)
         controller.healthService = mockHealthService.createMock()
-        controller.actuateSecurity = mockActuateSecurity.createMock()
 
         when:
         controller.health()
@@ -67,5 +60,54 @@ class HealthControllerSpec extends Specification {
         response.json.status == 'UP'
         !response.json.derbyDataSourceHealthIndicator // excluded from results since request not secure
         !response.json.h2DataSourceHealthIndicator // excluded from results since request not secure
+    }
+
+    void "request secure and user authorized"() {
+        given:
+        mockHealthService.demand.collectHealth() { -> return COLLECT_HEALTH_RESPONSE }
+
+        request.secure = true
+        request.addHeader(HttpHeaders.ACCEPT, JSON_CONTENT_TYPE)
+        controller.healthService = mockHealthService.createMock()
+        controller.actuateSecurity = new Authorized()
+
+        when:
+        controller.health()
+
+        then:
+        response.status == 200
+        response.json.status == 'UP'
+        response.json.derbyDataSourceHealthIndicator // included in results since request secure and user authorized
+        response.json.h2DataSourceHealthIndicator // included in results since request not and user authorized
+    }
+
+    void "request secure and user unauthorized"() {
+        given:
+        mockHealthService.demand.collectHealth() { -> return COLLECT_HEALTH_RESPONSE }
+
+        request.secure = true
+        request.addHeader(HttpHeaders.ACCEPT, JSON_CONTENT_TYPE)
+        controller.healthService = mockHealthService.createMock()
+        controller.actuateSecurity = new Unauthorized()
+
+        when:
+        controller.health()
+
+        then:
+        response.status == 401
+    }
+
+    class Authorized extends ActuateSecurity {
+        @Override
+        boolean isUserAuthorized(final Object request) {
+            true
+        }
+    }
+
+    class Unauthorized extends ActuateSecurity {
+        @Override
+        boolean isUserAuthorized(final Object request) {
+            false
+        }
     }
 }
