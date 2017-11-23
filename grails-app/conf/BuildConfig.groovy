782ba220c4be1d17a39cd5f8209f35e2782271a2
John Sampson <john.sampson@beeline.com>
2017-11-22 21:32:56 -0700

    Coverage: working out proper command for testing

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,14 +29,10 @@ def gebVersion = "0.13.1"
 def webdriverVersion = "2.53.1"
 
 grails.project.fork = [
-
-        // configure settings for the test-app JVM, uses the daemon by default
-        test   : [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon: true],
-        // configure settings for the run-app JVM
+        // configure settings for the test-app JVM, uses the daemon by default, test disables due to coverage needs
+        test: false,
         run    : [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
-        // configure settings for the run-war JVM
         war    : [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
-        // configure settings for the Console UI JVM
         console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
